fba2e2158058a997c76dd4365fdc8666d3b985ff
Fareena Firdaus <76551048+fareena08@users.noreply.github.com>
2022-11-06 22:27:16 +0800

    Add files via upload

---
+++ week6lab/db.php
@@ -0,0 +1,8 @@
+<?php
+ 
+$servername = "lrgs.ftsm.ukm.my";
+$username = "a181617";
+$password = "tinyorangesnake";
+$dbname = "a181617";
+ 
+?>
---
+++ week9lab/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Week 9 Lab Deliverables</title>
+</head>
+<body>
+  <center>
+    <h1 style="background-color:SkyBlue">Week 9 Lab (A181617)</h1>
+  </center>
+  <center>
+  <table width="50%" border="1" cellpadding="10"></center>
+  <tr>
+    <td bgcolor="powderBlue"><center><b>Main Menu<b></center></td>
+  </tr>
+  <tr>
+    
+    <td><center><a href="bio_form.php" target="_parent">Biodata Form</a></center></td>
+  </tr>
+  <tr>
+    <center>
+    <td><center><a href="content.php" target="_parent"> Stylesheet</a></center></td>
+  </tr>
+</table>
+
+</body>
+</html>
